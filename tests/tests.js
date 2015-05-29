9b9cd11fd1a8a1c83a4e87557c1b1577de62f405
Kristen Rieger <karieger1@gmail.com>
2015-05-28 20:42:45 -0500

    solving linking problems

---
+++ tests/tests.js
@@ -0,0 +1,94 @@
+/*var myvar = 'my value'; 
+
+(function() { 
+	var myvar = 'local value';
+    console.log(myvar);
+     
+})();
+*/
+
+/*var flag = true; 
+var flag = false;
+
+function test() {
+    if(flag) {
+       
+        console.log('Switch flag from true to false');
+    }
+    else {
+        flag = true;
+        console.log('Switch flag from false to true');
+    }
+}
+test();
+*/
+
+/*var message = 'Hello world'; 
+
+function saySomething() {
+    console.log(message);
+     message = 'Foo bar';
+}
+saySomething();
+*/
+
+/* var a = 1;
+function test() {
+    console.log(a);
+    console.log(foo());
+
+    var a = 1;
+    function foo() {
+        return 2;
+    }
+}
+
+test();
+*/
+
+/*(function() {
+	var bar = 1;
+    console.log(bar);
+    foo();
+
+    function foo() {
+        console.log('aloha');
+    }
+
+    baz = 2;
+})();
+*/
+
+// var run = false;
+
+// function fancy(arg1, arg2) {
+// 	  var run() {
+//         console.log('Will I run?');
+//     if(run) {
+//         console.log('I can run');
+//     }
+//     else {
+//         console.log('I can\'t run');
+//     }
+
+//    run() {
+//         console.log('Will I run?');
+//     }
+// }
+// fancy();
+
+var run = false;
+
+function fancy(arg1, arg2) {
+    if(run) {
+        console.log('I can run');
+    }
+    else {
+        console.log('I can\'t run');
+    }
+
+    var run = function() {
+        console.log('Will I run?');
+    }
+}
+fancy();
